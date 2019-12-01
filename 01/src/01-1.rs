2c18ebb9b5cdc6d3f9844ae92bb6049e0b0f85b1
Stefan Ackermann <stefan.ackermann42@gmail.com>
2019-12-01 20:36:10 +0100

    evolution

--- 01/src/01-1.rs
+++ 01/src/01-1.rs
@@ -1,19 +1,13 @@
 use std::fs;
-use std::str::Split;
+use std::error::Error;
 
-fn main() {
-    let file = &fs::read("data/input.txt");
-    let unwrap: Vec<u8> = file.as_ref().unwrap().to_vec();
-    let string_content: String = String::from_utf8_lossy(&unwrap).to_string();
-    let string_iter: Split<&str> = string_content.split("\n");
-    let inputs: Vec<i32> = string_iter
+fn main() -> Result<(), Box<dyn Error>>{
+    let file = fs::read("data/input.txt");
+    let sum: i32 = String::from_utf8_lossy(&file?).split("\n")
         .filter(|string| !string.is_empty())
-        .map(|item| item.parse().unwrap())
-        .collect();
-    
-    let sum = inputs.into_iter()
-        .map(|input| input / 3 - 2)
+        .map(|item| item.parse::<i32>().unwrap() / 3 - 2)
         .fold(0, |prev, cur| prev + cur);
 
     println!("{:?}", sum);
+    Ok(())
 }
--- 01/src/01-2.rs
+++ 01/src/01-2.rs
@@ -1,34 +1,28 @@
 use std::fs;
-use std::str::Split;
+use std::error::Error;
 
-fn main() {
-    let file = &fs::read("data/input.txt");
-    let unwrap: Vec<u8> = file.as_ref().unwrap().to_vec();
-    let string_content: String = String::from_utf8_lossy(&unwrap).to_string();
-    let string_iter: Split<&str> = string_content.split("\n");
-    let masses: Vec<i32> = string_iter
-        .filter(|string| !string.is_empty())
-        .map(|item| item.parse().unwrap())
-        .collect();
+fn main() -> Result<(), Box<dyn Error>>{
+    let string_content: String = fs::read_to_string("data/input.txt")?;
+    let masses: &mut dyn Iterator<Item = i32> = 
+        &mut string_content
+            .split("\n")
+           // .filter(|string| !string.is_empty())
+            .map(|item| item.parse::<i32>().unwrap());
     
     let fuel = calc_fuel(masses);
 
-    println
-    !("{:?}", fuel);
+    println!("{:?}", fuel);
+
+    Ok(())
 }
 
-fn calc_fuel(masses: Vec<i32>) -> i32{
-    let fuel = masses.into_iter()
-    .map(calc_fuel_for_mass)
-    .fold(0, |prev, cur| prev + cur);
-    return fuel;
+fn calc_fuel(masses: &mut dyn Iterator<Item = i32>) -> i32{
+    masses
+        .map(calc_fuel_for_mass)
+        .fold(0, |prev, cur| prev + cur)
 }
 
 fn calc_fuel_for_mass(mass: i32) -> i32 {
     let fuel = mass / 3 - 2;
-    if fuel < 0 {
-        return 0;
-    }else{
-        return fuel + calc_fuel_for_mass(fuel);
-    }
+    if fuel < 0 {0} else {fuel + calc_fuel_for_mass(fuel)}
 }
